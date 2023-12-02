9ef192ded7a56092245dd7583dda3d882f51309f
mubaarock <mustophamubarack17@gmail.com>
2023-12-02 23:47:44 +1100

    Complete solution to exercises in chapter 3

--- challenge_03.sql
+++ challenge_03.sql
@@ -1,3 +1,12 @@
 SELECT cust_name
 FROM Customers 
 ORDER BY cust_name DESC;
+
+SELECT cust_id , order_num
+FROM Orders
+ORDER BY cust_id, order_date DESC;
+
+SELECT quantity , item_price 
+FROM Orderitems
+ORDER BY quantity  DESC, item_price DESC
+LIMIT 8;
