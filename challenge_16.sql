ea63803c7ce29235242663a7e9913712df61ca11
mubaarock <mustophamubarack17@gmail.com>
2024-01-01 00:10:57 +1100

    Practice exercises on updating and deleting rows in a database table.

---
+++ challenge_16.sql
@@ -0,0 +1,18 @@
+-- Q2
+DELETE FROM Customers
+WHERE cust_id = 1000000009;SELECT * FROM CUSTOMERS
+
+
+-- Q1 A
+-- CREATE TABLE C AS SELECT * FROM Customers;
+-- UPDATE C
+-- SET cust_state = LOWER(cust_state)
+-- WHERE cust_country = "USA";
+-- SELECT * FROM C;
+
+-- Q1 B
+-- CREATE TABLE V AS SELECT * FROM Vendors;
+-- UPDATE V
+-- SET vend_state = LOWER(vend_state)
+-- WHERE vend_country = "USA";
+-- SELECT * FROM V;
