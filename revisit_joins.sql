ca560330d0e43e07a2381116c40c75b7bbc516bd
mubaarock <mustophamubarack17@gmail.com>
2024-01-04 09:46:34 +1100

    Resolve exercise 3 without using subqueries.

--- revisit_joins.sql
+++ revisit_joins.sql
@@ -1,3 +1,11 @@
+-- Q3
+SELECT prod_id,
+		order_date
+FROM Orders, Orderitems
+WHERE Orders.order_num = Orderitems.order_num
+	AND prod_id = "BR01";        
+
+-- Q2
 SELECT cust_name,
 		Orders.order_num,
         SUM(quantity*item_price) AS orderTotal
@@ -6,4 +14,6 @@ WHERE Customers.cust_id = Orders.cust_id
 	AND Orders.order_num = Orderitems.order_num
 GROUP BY Orders.order_num    
 ORDER BY orderTotal DESC;
+
+
         
